fafebdb19b2052db9034ca14efbea7fe4119c989
Mykol Benning <41842486+Mykolb@users.noreply.github.com>
2019-06-09 19:21:13 -0400

    Create very-easy.js

---
+++ very-easy.js
@@ -0,0 +1,14 @@
+
+//RETURN SUM OF TWO NUMBERS//
+//works
+function addition(a, b) {
+    return a + b;
+}
+console.log(addition(1500, 300));
+
+
+//ES6 syntax 
+//works
+let add = (a, b) => a + b;
+
+console.log(add(-10, 5));
