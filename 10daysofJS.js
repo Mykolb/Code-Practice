6e6397520a09a3a8829b65f3cfe9b02171299cc4
Mykol Benning <mykolbenning@Mykols-MBP.fios-router.home>
2020-08-22 01:04:19 -0400

    completed classes problem

--- 10daysofJS.js
+++ 10daysofJS.js
@@ -186,10 +186,44 @@ function getLetter(s) {
 }
 
 
-
-console.log(getLetter('rrs'))
-console.log(getLetter('mxy'))
-console.log(getLetter('bay'))
-
+// console.log(getLetter('rrs'))
+// console.log(getLetter('mxy'))
+// console.log(getLetter('bay'))
 
 
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+ // Create a polygon with side lengths 3, 4, and 5
+// let triangle = new Polygon([3, 4, 5]);
+
+// Print the perimeter
+// console.log(triangle.perimeter());
+// When executed with a properly implemented Polygon class, this code should print the result of.
+
+// DAY 4
+class Polygon {
+    constructor(length) {
+        this.length = length;
+    }
+
+
+    perimeter() {
+        let total = this.length.reduce((a, b) => a + b, 0)
+        return  total
+    }
+}
+
+
+
+let rec1 = new Polygon([1, 2, 3])
+let rec2 = new Polygon([10, 12, 5])
+
+
+
+console.log(rec1.perimeter())
+console.log(rec2.perimeter())
