67a8e63f968291a07d2ccd2017c001161a53be87
Mykol Benning <mykolbenning@Mykols-MBP.fios-router.home>
2020-09-28 22:11:03 -0400

    class problem done

--- edabit-classes.js
+++ edabit-classes.js
@@ -121,20 +121,15 @@ class Book {
     }
 }
 
-class Author extends Book {
-    constructor(name, books) {
-        super(books, name)
-        this.name = name;
-        this.books = books;
-
-    }
+let Author = {
+    name: this.name,
+    books: this.books,
 }
 
-class Publisher extends Author {
-    constructor(books, name) { 
-        super(books)
+let Publisher = new class {
+    constructor(authors, books) {
         this.authors = authors;
-        
+        this.books = books;
     }
 }
 
@@ -167,3 +162,57 @@ console.log(Bookstore.hasOwnProperty("books"), true, "The class Bookstore needs 
 console.log(Bookstore.hasOwnProperty("prices"), true, "The class Bookstore needs to have a value for prices.")
 }
 
+
+
+// Create a class that takes the following four arguements for a particular football player:
+
+// name
+// age
+// height
+// weight
+// Also, create three functions for the class that returns the following strings:
+
+// getAge() returns "name is age age"
+// getHeight() returns "name is heightcm"
+// getWeight() returns "name weighs weightkg"
+
+class Player {
+	constructor(name, age, height, weight) {
+        // complete function
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+	}
+	
+	getAge() {
+        // complete function
+        return `${this.name} is age ${this.age}`;
+	}
+
+	getHeight() {
+        // complete function
+        return `${this.name} is ${this.height}cm`;
+	}
+		
+	getWeight() {
+        // complete function
+       return `${this.name} weighs ${this.weight}kg`;
+	}
+}		
+
+player1 = new Player('Patrick Mahomes', 24, 188, 104)
+player2 = new Player('Jimmy Garoppolo', 28, 188, 102)
+player3 = new Player('Julio Jones', 31, 191, 100)
+
+console.log(player1.getAge(), 'Patrick Mahomes is age 24')
+console.log(player1.getHeight(), 'Patrick Mahomes is 188cm')
+console.log(player1.getWeight(), 'Patrick Mahomes weighs 104kg')
+
+console.log(player2.getAge(), 'Jimmy Garoppolo is age 28')
+console.log(player2.getHeight(), 'Jimmy Garoppolo is 188cm')
+console.log(player2.getWeight(), 'Jimmy Garoppolo weighs 102kg')
+
+console.log(player3.getAge(), 'Julio Jones is age 31')
+console.log(player3.getHeight(), 'Julio Jones is 191cm')
+console.log(player3.getWeight(), 'Julio Jones weighs 100kg')
