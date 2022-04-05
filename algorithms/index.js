889472599a2609673c025c9a577103d35088c0ce
Mykol Benning <mykolbenning@Mykols-MBP.lan>
2022-04-04 23:26:46 -0400

    completed problem

---
+++ algorithms/index.js
@@ -0,0 +1,9 @@
+// Convert Age to Days
+// Create a function that takes the age in years and returns the age in days.
+const calcAge = age => {
+	return age*365
+}
+
+console.log(calcAge(10), 3650)
+console.log(calcAge(0), 0)
+console.log(calcAge(73), 26645)
