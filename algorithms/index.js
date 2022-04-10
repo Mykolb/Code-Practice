c1a8127b348bb30eac386299197c6df5694c8768
Mykol Benning <mykolbenning@Mykols-MBP.lan>
2022-04-10 18:13:54 -0400

    problem solved

--- algorithms/index.js
+++ algorithms/index.js
@@ -208,3 +208,32 @@ console.log(programmers(33,  72,  74),  41)
 console.log(programmers(147, 33,  526), 493)
 console.log(programmers(234, 345, 457), 223)
 
+// Which Function Returns the Larger Number?
+// Your function will be passed two functions, f and g, that don't take any parameters. 
+// Your function has to call them, and return a string which indicates which function returned the larger number.
+
+// If f returns the larger number, return the string f.
+// If g returns the larger number, return the string g.
+// If the functions return the same number, return the string neither.
+function whichIsLarger(f, g) {
+	const fFunc = f()
+    const gFunc = g()
+    console.log(fFunc)
+    console.log(gFunc)
+
+    if(fFunc > gFunc) {
+        return 'f'
+    } else if(gFunc > fFunc){
+        return 'g'
+    } else {
+        return 'neither'
+    }
+}
+
+console.log(whichIsLarger(()=>5, ()=>10),'g')
+console.log(whichIsLarger(()=>10, ()=>5),'f')
+console.log(whichIsLarger(()=>25, ()=>25),'neither')
+console.log(whichIsLarger(()=>-100, ()=>-100),'neither')
+console.log(whichIsLarger(()=>-100, ()=>0),'g')
+console.log(whichIsLarger(()=>505050, ()=>5050),'f')
+console.log(whichIsLarger(()=>100, ()=>1000),'g')
